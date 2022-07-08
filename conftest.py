d25fe4286cc8ad0b97919495e0c59fc6c421f5a1
kirill-linux <serdyukovkirill2@gmail.com>
2022-07-08 17:36:28 +0300

    prerare env and create conftest with BasePage and also wrote my first test

---
+++ conftest.py
@@ -0,0 +1,20 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+import fake_useragent
+
+user = fake_useragent.UserAgent().random
+
+
+@pytest.fixture(scope="function")
+def driver():
+    print("\nstart browser")
+    option = Options()
+    option.add_argument('disable-notifications')  # выключаем оповещения
+    option.headless = False  # В фоновом режиме
+    option.add_argument(f"user-agent={user}")  # подменить user-agent
+    driver = webdriver.Chrome(options=option)
+    # driver.maximize_window()
+    yield driver
+    print("\nquit browser")
+    driver.quit()
